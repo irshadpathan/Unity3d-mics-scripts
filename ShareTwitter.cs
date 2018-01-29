0e14b8bec71a4f8b9a1625068f4d042effbab667
irshad khan <irshadpathan@users.noreply.github.com>
2018-01-29 17:48:35 +0530

    Add files via upload

---
+++ ShareTwitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareTwitter : MonoBehaviour {
+
+	public string massage;
+	public string url;
+	public string hashtags;
+	public string senduser;
+
+
+	public void TwitterShare()
+	{
+		TWShare ();
+	}
+
+
+	 void TWShare()
+	{
+		Application.OpenURL 
+		(
+			"https://twitter.com/intent/tweet?text=" + massage + "&url=" + url + "&hashtags=" + hashtags + "&via=" + senduser + "&related=twitter"
+		);
+	}
+
+}
